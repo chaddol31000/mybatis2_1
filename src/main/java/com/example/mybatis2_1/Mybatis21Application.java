f171f941426f2ddf923b6abfed734ddc3af5bf70
차하연 <chaddol31000@naver.com>
2025-04-07 09:35:41 +0900

    프로젝트 최초 배포

---
+++ src/main/java/com/example/mybatis2_1/Mybatis21Application.java
@@ -0,0 +1,13 @@
+package com.example.mybatis2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatis21Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Mybatis21Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/mybatis2_1/Mybatis21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatis2_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatis21ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
